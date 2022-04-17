ee4412869b162f6df664290857055df5ca48d99e
Gladky Dmitry <dmitrygladky@yandex-team.ru>
2022-04-17 17:16:40 +0300

    add go backend

---
+++ back/cmd/auth/go.mod
@@ -0,0 +1,9 @@
+module github.com/Miktor/make.todo/back
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go/v4 v4.0.0-preview1 // indirect
+	github.com/labstack/echo/v4 v4.7.2 // indirect
+	github.com/spf13/viper v1.11.0 // indirect
+)
---
+++ back/cmd/auth/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/register", handler)
+	http.HandleFunc("/login", handler)
+	http.HandleFunc("/refresh-token", handler)
+	log.Print("Starting...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
